b0ac59436118eaebae2a3e33ea01aa9e0382af34
srohithadithya <rohithadithyas2005@gmail.com>
2026-01-11 17:42:20 +0530

    Fix: Update Provider App API port to 8001

--- mobile-provider/src/api/client.ts
+++ mobile-provider/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const BASE_URL = 'http://10.0.2.2:8000/api/v1';
+const BASE_URL = 'http://10.0.2.2:8001/api/v1';
 
 export const apiClient = axios.create({
     baseURL: BASE_URL,
